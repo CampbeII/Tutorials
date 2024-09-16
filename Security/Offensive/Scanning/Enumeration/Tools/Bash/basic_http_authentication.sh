cae640a794891ddbd3e2484905b74ee4e4240913
Automation <alexander.campbell@hrsdc-rhdcc.gc.ca>
2024-09-16 08:23:51 -0300

    basic auth

--- Security/Offensive/Scanning/Enumeration/Tools/Bash/basic_http_authentication.sh
+++ Security/Offensive/Scanning/Enumeration/Tools/Bash/basic_http_authentication.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-
+endpoint=''
+password=''
+username='admin'
+while IFS='' read password 
+do
+    encoded=$("$username:$password" | base64)
+    response=$(curl -s -L -H "Authorization: Basic $endoded")
+done < passwords.txt
